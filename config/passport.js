5b92f1ef31efdbe4b2de15ed0a1441ed978423cd
Kévin Bizien <bizienkevin@gmail.com>
2014-10-28 21:17:59 +0100

    configure & prepare simple authentification with passport

---
+++ config/passport.js
@@ -0,0 +1,14 @@
+var passport = require('passport');
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// Bootstrap local config
+require('./strategies/local')();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/login', function(req, res) {
   res.render('login', { title: 'Login' });
+});
+
+router.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  })
+);
+
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
