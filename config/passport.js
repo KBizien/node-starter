4e7e3d3fc8db96a2aa2864433a5e28b2217fc2f0
Kévin Bizien <bizienkevin@gmail.com>
2014-11-02 18:22:17 +0100

    login & signup work (less crypt password)

--- config/passport.js
+++ config/passport.js
@@ -1,9 +1,24 @@
+// load all the things we need
+var LocalStrategy   = require('passport-local').Strategy;
+
+// load up the user model
+var models = require('../models');
+var User = models.sequelize.import('../../../models/user');
+
+// expose this function to our app using module.exports
 module.exports = function(passport) {
-  // Serialize sessions
+// =========================================================================
+  // passport session setup ==================================================
+  // =========================================================================
+  // required for persistent login sessions
+  // passport needs ability to serialize and unserialize users out of session
+
+  // used to serialize the user for the session
   passport.serializeUser(function(user, done) {
     done(null, user.id);
   });
 
+  // used to deserialize the user
   passport.deserializeUser(function(id, done) {
     db.User.find({where: {id: id}}).success(function(user){
       done(null, user);
@@ -12,6 +27,77 @@ module.exports = function(passport) {
     });
   });
 
-// Bootstrap local config
-require('./strategies/local')(passport);
+// =========================================================================
+  // LOCAL LOGIN =============================================================
+  // =========================================================================
+  // we are using named strategies since we have one for login and one for signup
+  // by default, if there was no name, it would just be called 'local'
+
+  passport.use('local-login', new LocalStrategy({
+      // by default, local strategy uses username and password, we will override with email
+      usernameField : 'username',
+      passwordField : 'password',
+      passReqToCallback : true // allows us to pass back the entire request to the callback
+  },
+  function(req, username, password, done) { // callback with email and password from our form
+
+      // find a user whose email is the same as the forms email
+      // we are checking to see if the user trying to login already exists
+      User.findOne({ where: { username: username }}).success(function(user) {
+        if (!user) {
+          console.log('Unknown user');
+          return done(null, false, req.flash('error', 'Unknown user'));
+        } else if (password != user.password) {
+          console.log('Invalid password');
+          return done(null, false, req.flash('error', 'Invalid password'));
+        } else {
+          console.log('Welcome '+ user.username);
+          return done(null, user, req.flash('success', 'Welcome !'));
+        }
+      }).error(function(err){
+        return done(err);
+      });
+
+  }));
+
+// =========================================================================
+  // LOCAL SIGNUP ============================================================
+  // =========================================================================
+  // we are using named strategies since we have one for login and one for signup
+  // by default, if there was no name, it would just be called 'local'
+
+  passport.use('local-signup', new LocalStrategy({
+      // by default, local strategy uses username and password, we will override with email
+      usernameField : 'username',
+      passwordField : 'password',
+      passReqToCallback : true // allows us to pass back the entire request to the callback
+  },
+  function(req, username, password, done) {
+      // asynchronous
+      // User.findOne wont fire unless data is sent back
+      process.nextTick(function() {
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        User.findOne({ where:{ username:  username }}).success(function(user) {
+            // check to see if theres already a user with that email
+            if (user) {
+                console.log('That username is already taken');
+                return done(null, false, req.flash('error', 'That username is already taken.'));
+            } else {
+              // if there is no user with that username
+              // save the user
+              User
+                .create({
+                  username: username,
+                  password: password
+                })
+                .complete(function(err, user) {
+                  if (err)
+                    throw err;
+                  return done(null, user);
+                })
+            }
+        });
+      });
+  }));
 };
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,60 @@
 //var express = require('express');
 //var router = express.Router();
-//svar passport = require('passport');
+//var passport = require('passport');
 
 module.exports = function(app, passport) {
-  /* GET home page. */
+
+  // =====================================
+  // HOME PAGE (with login links) ========
+  // =====================================
   app.get('/', function(req, res) {
-    console.log(req.flash('succes'));
-    res.render('index', { title: 'Express', success: req.flash('success') });
+    var success = req.flash('success');
+    console.log(success);
+    res.render('index.ejs', { message: success });
   });
 
+  // =====================================
+  // LOGIN ===============================
+  // =====================================
+  // show the login form
   app.get('/login', function(req, res) {
-    console.log(req.flash('error'));
-    res.render('login', { title: 'Login', error: req.flash('error') });
+    var error = req.flash('error');
+    console.log(error);
+    res.render('login.ejs', { message: error });
   });
 
+  // process the login form
   app.post('/login',
-    passport.authenticate('local-signin', {
+    passport.authenticate('local-login', {
       successRedirect: '/',
       failureRedirect: '/login',
       failureFlash: true,
       successFlash: true
     })
   );
+
+  // =====================================
+  // SIGNUP ==============================
+  // =====================================
+  // show the signup form
+  app.get('/signup', function(req, res) {
+    // render the page and pass in any flash data if it exists
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
+  });
+
+  // process the signup form
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect : '/',
+    failureRedirect : '/signup',
+    failureFlash : true
+  }));
 };
 
-//module.exports = router;
+//route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+  // if user is authenticated in the session, carry on
+  if (req.isAuthenticated())
+    return next();
+  // if they aren't redirect them to the home page
+  res.redirect('/');
+}
