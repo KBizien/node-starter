21a702054dc88b65cfdf4e7b1e99efff6a2e1e69
Kévin Bizien <bizienkevin@gmail.com>
2014-11-17 21:42:30 +0100

    add dotting module

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var path = require('path');
 var passport = require('passport');
 var flash = require('connect-flash');
 
+var dotenv = require('dotenv');
+dotenv.load();
+
 var logger = require('morgan');
 var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
